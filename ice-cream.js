5503a6dbb4ed87b7ea29bd9f51495ce51c2b8d75
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-10-27 15:25:29 -0700

    ice cream

---
+++ ice-cream.js
@@ -0,0 +1,5 @@
+// Do not modify these first two lines
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root beer")
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
