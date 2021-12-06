6178bed1611529ec5c589cb4894108b1547a0e29
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2021-12-05 19:58:03 -0800

    Eslint

--- bonus/Queen.js
+++ bonus/Queen.js
@@ -1,125 +1,106 @@
-function generateBoard (whiteQueen, blackQueen) {
-    let board = []
-    let temp = []
-    
-    for(let row = 0; row < 8; row++) {
-        for(let column = 0; column < 8; column++) {
-            temp.push('0') 
-            if(whiteQueen[0] === row) {    
-                if(whiteQueen[1] === column){
-                    temp.pop()
-                    temp.push('1')   
-                }
-            }
-            else if(blackQueen[0] === row) {
-                if(blackQueen[1] === column) {
-                    temp.pop()
-                    temp.push('1')
-                }
-            }   
+const generateBoard = (whiteQueen, blackQueen) => {
+  let board = [];
+  let temp = [];
+  for (let row = 0; row < 8; row++) {
+    for (let column = 0; column < 8; column++) {
+      temp.push('0');
+      if (whiteQueen[0] === row) {
+        if (whiteQueen[1] === column) {
+          temp.pop();
+          temp.push('1');
         }
-        
-        board.push(temp)
-        temp = []
-    }return board
-}
-function queenThreat(board){
-    let result = false;
-    const FirstQueen = (board) => {
-        for (let i = 0; (i) < board.length; (i)++) {
-            for (let j = 0; j < board.length; j++) {
-                if(board[i][j] === '1'){
-                    return([i,j])
-                }
-            }
-        }   
+      } else if (blackQueen[0] === row) {
+        if (blackQueen[1] === column) {
+          temp.pop();
+          temp.push('1');
+        }
+      }
     }
-    let chess = FirstQueen(board);
-    let x = whiteQueen[0]
-    let y = whiteQueen[1]
-
-    function vertical() {
-        for (let i = 0; i < 8; i++) {
-            board[x][i] = 'q';
-            if(board[x][i] === '1' && i != y) {
-                board[x][i] = 'q'
-                return result = true
-            }
+    board.push(temp);
+    temp = [];
+  }
+  return board;
+};
+const queenThreat = (board) => {
+  let result = false;
+  const FirstQueen = (board) => {
+    for (let i = 0; (i) < board.length; (i)++) {
+      for (let j = 0; j < board.length; j++) {
+        if (board[i][j] === '1') {
+          return ([i,j]);
         }
+      }
     }
-    
-    function horizontal() {
-        for(let i = 0; i < board.length; i ++) {
-            board[i][y] = 'q'
-            if(board[i][y] === '1' && i != x) {
-                board[i][y] = 'q'
-                return result = true
-            }
-        }
+  };
+  let chess = FirstQueen(board);
+  let x = whiteQueen[0];
+  let y = whiteQueen[1];
+
+  const vertical = () => {
+    for (let i = 0; i < 8; i++) {
+      board[x][i] = 'q';
+      if (board[x][i] === '1' && i !== y) {
+        board[x][i] = 'q';
+        return result = true;
+      }
     }
+  };
 
-    function crossForward() {
-        let initialx = x;
-        let initialy = y;
-        for (initialx; 0 < initialx  && initialy > 0; initialx--, initialy--){
-            board[initialx][initialy] = 'Q'
-            if (board[initialx][initialy] === 1 && initialx != x && initialy != y){
-                board[initialx][initialy] = 'Q'
-                result = true; 
-                return result; 
-            };
-        }
-         for (initialx, initialy; initialx < 8 && initialy < 8; initialx++, initialy++) {
-          board[initialx][initialy] = 'Q'
-          if (board[initialx][initialy] === 1 && initialx != x && initialy != y){
-            board[initialx][initialy] = 'Q'
-            result = true; 
-            return result; 
-          };
-        }
+  const horizontal = () => {
+    for (let i = 0; i < board.length; i ++) {
+      board[i][y] = 'q';
+      if (board[i][y] === '1' && i !== x) {
+        board[i][y] = 'q';
+        return result = true;
+      }
     }
-    function corssReverse(){
-        let initialx = x;
-        let initialy = y;
-        for(initialx; 7 > initialx && initialy > 0; initialx++, initialy--) {
-            board[initialx][initialy] = 'Q'
-            if (board[initialx][initialy] === 1 && initialx != x && initialy != y){
-                board[initialx][initialy] = 'Q'
-                result = true; 
-                return result; 
-            };
-        }
-        for (initialx, initialy; 0 < initialx && initialy > 8; initialx--, initialy++) {
-            board[initialx][initialy] = 'Q'
-            if(board[initialx][initialy] === '1' && initialx != x && initialy != y) {
-                board[initialx][initialy] = 'Q'
-                return result = true
-            }
-        }
-        
-        
+  };
+
+  const crossForward = () => {
+    let initialx = x;
+    let initialy = y;
+    for (initialx; 0 < initialx  && initialy > 0; initialx--, initialy--) {
+      board[initialx][initialy] = 'Q';
+      if (board[initialx][initialy] === 1 && initialx !== x && initialy !== y) {
+        board[initialx][initialy] = 'Q';
+        result = true;
+        return result;
+      }
     }
-    vertical(board)
-    horizontal(board)
-    crossForward(board)
-    corssReverse(board)
-    if(result === true){
-        return result
+    for (initialx, initialy; initialx < 8 && initialy < 8; initialx++, initialy++) {
+      board[initialx][initialy] = 'Q';
+      if (board[initialx][initialy] === 1 && initialx !== x && initialy !== y) {
+        board[initialx][initialy] = 'Q';
+        result = true;
+        return result;
+      }
     }
-    return result
-    
-}
-
-
-
-// let whiteQueen = [0, 5];
-// let blackQueen = [5, 0];
-//  let generatedBoard = generateBoard(whiteQueen, blackQueen);
-//  console.log(generatedBoard);
-// console.log(queenThreat(generatedBoard));
-
-let whiteQueen = [0, 2];
-let blackQueen = [5, 7];
-let generatedBoard = generateBoard(whiteQueen, blackQueen);
-console.log(generatedBoard);
-console.log(queenThreat(generatedBoard));
+  };
+  const corssReverse = () => {
+    let initialx = x;
+    let initialy = y;
+    for (initialx; 7 > initialx && initialy > 0; initialx++, initialy--) {
+      board[initialx][initialy] = 'Q';
+      if (board[initialx][initialy] === 1 && initialx !== x && initialy !== y) {
+        board[initialx][initialy] = 'Q';
+        result = true;
+        return result;
+      }
+    }
+    for (initialx, initialy; 0 < initialx && initialy > 8; initialx--, initialy++) {
+      board[initialx][initialy] = 'Q';
+      if (board[initialx][initialy] === '1' && initialx !== x && initialy !== y) {
+        board[initialx][initialy] = 'Q';
+        return result = true;
+      }
+    }
+  };
+  vertical(board);
+  horizontal(board);
+  crossForward(board);
+  corssReverse(board);
+  if (result === true) {
+    return result;
+  }
+  return result;
+};
